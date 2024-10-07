d0762caa6c51c4e768d2dd246798150ad80c4e32
Mohanad <mo.arcade@gmail.com>
2024-10-07 13:45:19 -0400

    Final Render

--- src/main.cc
+++ src/main.cc
@@ -95,7 +95,7 @@
 
 int main() {
 
-    /* Old Test Window
+    /* Playground Main Function
         // int imageWidth = 256;
         // int imageHeight = 256;
 
@@ -159,11 +159,10 @@ int main() {
         // }
         // // Print Done and clear any remaining text from the progress message
         // std::clog << "\rDone.                 \n";
-*/
     
     // Scene Setup
-    // Creates a hittableList object world, which will store all objects in the scene that can be hit by rays
-    hittableList world;
+    // Creates a hittableList object world, which will store all objects in the scene that can be hit by rays 
+     hittableList world;
 
     // Testing materials onto the objects
     auto materialGround = make_shared<lambertian>(color(0.8, 0.8, 0.0));
@@ -179,19 +178,19 @@ int main() {
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, materialBubble));
     world.add(make_shared<sphere>(point3(1.0, -0.0, -1.0), 0.5, materialRight)); 
 
-    /* Old sphere to the world at the position (0,0,-1) with a radius of 0.5
+    // Old sphere to the world at the position (0,0,-1) with a radius of 0.5
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
     // Adds a large sphere with a radius of 100 to the scene centered at (0,-100.5,-1). This sphere acts as the ground
-    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100)); */
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100)); 
 
-    /* // Testing Positionable Camera
+    // Testing Positionable Camera
     auto R = std::cos(pi/4);
 
     auto materialLeft  = make_shared<lambertian>(color(0,0,1));
     auto materialRight = make_shared<lambertian>(color(1,0,0));
 
     world.add(make_shared<sphere>(point3(-R, 0, -1), R, materialLeft));
-    world.add(make_shared<sphere>(point3( R, 0, -1), R, materialRight)); */
+    world.add(make_shared<sphere>(point3( R, 0, -1), R, materialRight)); 
 
     // Camera Setup
     // Creates a camera object which wuill handle the rendering process by shooting rays into the scene
@@ -217,5 +216,65 @@ int main() {
     // Calls the render function on the cam object to begin rendering the scene
     // The world object is passed as a parameter, containing all the objects to render
     // The camera shoots rays from its position through each pixel in the image and checks for intersections with objects in the world to determine the color of each pixel
+    cam.render(world); */
+
+    // Final Render 
+        hittableList world;
+
+    auto groundMaterial = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0,-1000,0), 1000, groundMaterial));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto chooseMat = randomDouble();
+            point3 center(a + 0.9*randomDouble(), 0.2, b + 0.9*randomDouble());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphereMaterial;
+
+                if (chooseMat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphereMaterial = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                } else if (chooseMat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = randomDouble(0, 0.5);
+                    sphereMaterial = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                } else {
+                    // glass
+                    sphereMaterial = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphereMaterial));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    camera cam;
+
+    cam.aspectRatio      = 16.0 / 9.0;
+    cam.imageWidth       = 1200;
+    cam.samplesPerPixel  = 500;
+    cam.maxDepth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookFrom = point3(13,2,3);
+    cam.lookAt   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocusAngle = 0.6;
+    cam.focusDist    = 10.0;
+
     cam.render(world);
 }
